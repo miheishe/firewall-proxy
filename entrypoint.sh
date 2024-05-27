9b07537c6137406e0936322fcb0a3e4ed1fb724a
miheishe <dael@bk.ru>
2024-05-27 11:39:09 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
 
-# Файл со списком разрешенных IP-адресов
+# Файл со списком разрешенных доменов
+ALLOWED_DOMAINS_FILE="/etc/squid/allowed_hosts.txt"
 IP_LIST_FILE="/usr/local/bin/ip_list.txt"
 
 # Очистка текущих правил iptables
 iptables -F
 iptables -X
 
-# Разрешение трафика к указанным IP-адресам
-while IFS= read -r IP
+# Очистка файла ip_list.txt
+> "$IP_LIST_FILE"
+
+# Разрешение трафика к указанным доменам
+while IFS= read -r DOMAIN
 do
     # Пропуск пустых строк и строк, начинающихся с #
-    if [[ -n "$IP" && "$IP" != \#* ]]; then
-        iptables -A OUTPUT -d "$IP" -j ACCEPT
-        iptables -A INPUT -s "$IP" -j ACCEPT
+    if [[ -n "$DOMAIN" && "$DOMAIN" != \#* ]]; then
+        # Получаем IP-адреса для домена
+        IPS=$(dig +short "$DOMAIN")
+
+        for IP in $IPS; do
+            echo "$IP" >> "$IP_LIST_FILE"
+            iptables -A OUTPUT -d "$IP" -j ACCEPT
+            iptables -A INPUT -s "$IP" -j ACCEPT
+        done
     fi
-done < "$IP_LIST_FILE"
+done < "$ALLOWED_DOMAINS_FILE"
 
 # Отбрасывание всего остального трафика
 iptables -A OUTPUT -j DROP
